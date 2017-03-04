6f5eefae982ef3233a0000108d6212556d6edd8c
Alex Kearney <konga.kearney@gmail.com>
2017-03-04 14:25:09 -0700

    fixed the stream error

--- rss_crawler.py
+++ rss_crawler.py
@@ -5,7 +5,8 @@ from ConfigParser import ConfigParser
 from slugify import slugify
 import os
 from contextlib import closing
-
+import datetime
+from newspaper.article import ArticleException
 
 __author__ = 'kongaloosh'
 
@@ -24,76 +25,78 @@ if not os.path.isfile("news.db"):
 conn = sqlite3.connect(DATABASE)
 c = conn.cursor()
 
-ctv_url = 'http://edmonton.ctvnews.ca/'
-print("building CTV")
-ctv = newspaper.build(ctv_url, memoize_articles=False)
-print(ctv)
+for source in ['http://edmonton.ctvnews.ca/', 'http://globalnews.ca/edmonton/', 'http://edmontonjournal.com/']:
+    print('building ' + source)
+    paper = newspaper.build(source, memoize_articles=False)
+    for article in paper.articles:
+        try:
+            article.download()
+            article.parse()
+            if len(article.authors) == 0:
+                authors = None
+            else:
+                try:
+                    authors = article.authors[0] + ", ".join(authors[1:])
+                except IndexError:
+                    authors = article.authors[0]
+                except AttributeError:
+                    authors = article.authors[0]
+                except TypeError:
+                    authors = article.authors[0]
 
+            title = article.title
+            published = article.publish_date
+            if published is None:
+                published = datetime.datetime.now()
+            slug = slugify(title)
+            try:
+                image = article.images[0]
+            except IndexError:
+                image = None
+            article.nlp()
+            summary = article.summary
+            keywords = article.keywords
+            url = article.url
+            location = None
 
-for category in ctv.category_urls():
-    print(category)
-
-for article in ctv.articles:
-    article.download()
-    article.parse()
-    print(article.authors)
-
-    try:
-        authors = article.authors[0] + ", ".join(authors[1:])
-    except IndexError:
-        authors = None
-    except AttributeError:
-        authors = article.authors[0]
-
-    title = article.title
-    published = article.publish_date
-    slug = slugify(title)
-    try:
-        image = article.images[0]
-    except IndexError:
-        image = None
-    article.nlp()
-    summary = article.summary
-    keywords = article.keywords
-    url = article.url
-    location = None
-
-    """
-        create table articles (
-          id integer primary key autoincrement,
-          slug text not null,
-          summary text not null,
-          author text,
-          url text not null,
-          image text,
-          published date not null,
-          location text,
-        );
-    """
-
-    c.execute(
-        """
-            insert into articles
-            (slug, summary, author, url, image, published, location) values (?,?,?,?,?,?)
-
-        """,
-        [slug, summary, authors, url, image, published, location ]
-    )
-
-    """
-    create table categories(
-      id integer primary key autoincrement,
-      slug text not null,
-      published date not null,
-      category text not null,
-      FOREIGN KEY (slug) REFERENCES articles(slug)
-    );
-    """
-    for key in keywords:
-        c.execute(
             """
-                insert into categories (slug, published, category) values (slug, published, category)
-            """,
-            [slug, published, key]
-        )
+                create table articles (
+                  id integer primary key autoincrement,
+                  slug text not null,
+                  summary text not null,
+                  author text,
+                  url text not null,
+                  image text,
+                  published date not null,
+                  location text,
+                );
+            """
 
+            c.execute(
+                """
+                    insert into articles
+                    (slug, summary, author, url, image, published, location) values (?,?,?,?,?,?,?)
+                """,
+                [slug, summary, authors, url, image, published, location]
+            )
+
+            """
+            create table categories(
+              id integer primary key autoincrement,
+              slug text not null,
+              published date not null,
+              category text not null,
+              FOREIGN KEY (slug) REFERENCES articles(slug)
+            );
+            """
+            conn.commit()
+            for key in keywords:
+                c.execute(
+                    """
+                        insert into categories (slug, published, category) values (?,?,?)
+                    """,
+                    [slug, published, key]
+                )
+                conn.commit()
+        except ArticleException:
+            print("ARTICLE EXCEPTION")
